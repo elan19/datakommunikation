05e5714e2c1d7c217848c77d321b2a5bc1e61ad5
elan19 <elan19@student.bth.se>
2021-02-15 18:23:03 +0100

    fixat så jag får tillbaka ett svar från server

--- clientmain.cpp
+++ clientmain.cpp
@@ -19,7 +19,8 @@
 // Included to get the support library
 #include "calcLib.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
   /*
     Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
@@ -89,71 +90,137 @@ int main(int argc, char *argv[]){
   int bytes;
 
 
-if((bytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
-{
-  printf("%s\n",strerror(errno));
-  close(sockfd);
-  exit(0);
-}
+  if((bytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
+  {
+   printf("%s\n",strerror(errno));
+    close(sockfd);
+    exit(0);
+  }
 
-printf("%s\n", buf);
+  printf("%s\n", buf);
 
-if(strstr(buf, PROTOCOL) == NULL)
-{
-  printf("Wrong protocol.\n");
-  close(sockfd);
-  exit(0);
-}
+  if(strstr(buf, PROTOCOL) == NULL)
+  {
+    printf("Wrong protocol.\n");
+    close(sockfd);
+    exit(0);
+  }
 
-printf("Accepted protocol\n");
+  printf("Accepted protocol\n");
 
-if(send(sockfd, "OK\n", strlen("OK\n"),0) == -1)
-{
-  printf("Error: Couldnt send\n");
-  close(sockfd);
-  exit(0);
-}
+  if(send(sockfd, "OK\n", strlen("OK\n"),0) == -1)
+  {
+    printf("Error: Couldnt send\n");
+    close(sockfd);
+    exit(0);
+  }
 
-memset(buf,0,128);
+  memset(buf,0,128);
 
-if((bytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
-{
-  printf("%s\n",strerror(errno));
-  close(sockfd);
-  exit(0);
-}
+  if((bytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
+  {
+    printf("%s\n",strerror(errno));
+    close(sockfd);
+    exit(0);
+  }
 
-printf("%s\n", buf);
+  printf("%s\n", buf);
 
- char operation[5];
+  char operation[5];
 
-if(buf[0] == 'f')
-{
-  double value1 = 0;
-  double value2 = 0;
+  if(buf[0] == 'f')
+  {
+    double value1 = 0;
+    double value2 = 0;
+    double total = 0;
 
-  sscanf(buf, "%s %lf %lf", operation, &value1, &value2);
-  printf("%lf %lf\n", value1, value2);
+    sscanf(buf, "%s %lf %lf", operation, &value1, &value2);
 
-}
-else
-{
-  int value1 = 0;
-  int value2 = 0;
+    if(strstr(operation, "fadd"))
+    {
+      total = value1 + value2;
+    }
+    else if(strstr(operation, "fdiv"))
+    {
+      total = value1 / value2;
+    }
+    else if(strstr(operation, "fmul"))
+    {
+      total = value1 * value2;
+    }
+    else
+    {
+      total = value1 - value2;
+    }
 
-  sscanf(buf, "%s %d %d", operation, &value1, &value2);
+    printf("%lf\n", total);
 
-  printf("%d %d\n", value1, value2);
+    char answ[5];
+    sprintf(answ, "%lf\n", total);
 
-}
+    if(send(sockfd, answ, strlen(answ),0) == -1) 
+  {
+    printf("Error: Couldnt send\n");
+    close(sockfd);
+    exit(0);
+  }
+
+  }
+  else
+  {
+    int value1 = 0;
+    int value2 = 0;
+    int total = 0;
+
+    sscanf(buf, "%s %d %d", operation, &value1, &value2);
+
+    if(strstr(operation, "add"))
+    {
+      total = value1 + value2;
+    }
+    else if(strstr(operation, "div"))
+    {
+      total = value1 / value2;
+    }
+    else if(strstr(operation, "mul"))
+    {
+      total = value1 * value2;
+    }
+    else
+    {
+      total = value1 - value2;
+    }
+
+    printf("%d\n", total);
+
+    char answ[3];
+    sprintf(answ, "%d\n", total);
+
+    if(send(sockfd, answ, strlen(answ),0) == -1)  
+    {
+      printf("Error: Couldnt send\n");
+      close(sockfd);
+      exit(0);
+    }
+
+  }
+
+  memset(buf,0,128);
+
+  if((bytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
+  {
+    printf("%s\n",strerror(errno));
+    close(sockfd);
+    exit(0);
+  }
+
+  printf("%s\n", buf);
 
 
+  #ifdef DEBUG 
+    printf("Host %s, and port %d.\n",Desthost,port);
+  #endif
 
-
-#ifdef DEBUG 
-  printf("Host %s, and port %d.\n",Desthost,port);
-#endif
-
-  close(sockfd);
-  return 0;
+    close(sockfd);
+    return 0;
 }
